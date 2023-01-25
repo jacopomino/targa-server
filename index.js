0f28c30c52e975086f9eb3b3a23094753e1aabea
jacopomino <92917960+jacopomino@users.noreply.github.com>
2023-01-25 18:24:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import { MongoClient} from "mongodb"
+
+
+const PORT = process.env.PORT|| 3001;
+const app=express()
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.listen(PORT,()=>{
+    console.log("run");
+})
+
+//parte dei clienti
+app.get("/dati", async (req,res)=>{
+  MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("targa");
+    dbo.collection("users").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.send(result)
+    });
+  });
+})
+app.put("/signup", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("targa");
+      dbo.collection("users").insertOne(info,(err,result)=>{
+      if (err) throw err;
+      res.send(result)
+      })
+    });
+})
