2b599c4e87dee7c1de4c3d2880bbfc6283d00882
jacopomino <92917960+jacopomino@users.noreply.github.com>
2023-02-15 10:50:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 import { MongoClient,ObjectId} from "mongodb"
+import multer from "multer"
+import { createReadStream,readFile,writeFileSync } from 'fs'
+import path from "path"
+
 
 
 const PORT = process.env.PORT|| 3001;
@@ -11,6 +15,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.listen(PORT,()=>{
     console.log("run");
 })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage });
 
 //parte dei clienti
 app.get("/dati", async (req,res)=>{
@@ -94,6 +100,68 @@ app.put("/send-message", async (req,res)=>{
           if (err) throw err;
         })
       }
+    })
+  });
+})
+app.put("/like", async (req,res)=>{
+  let info=JSON.parse(Object.keys(req.body)[0]);
+  MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("targa");
+    dbo.collection("users").find({_id:new ObjectId(info.id)}).toArray(function(err, result) {
+      if(result[0].giudizio){
+        dbo.collection("users").updateOne({_id:new ObjectId(info.id)},{$set:{giudizio:{like:result[0].giudizio.like+1,dislike:result[0].giudizio.dislike}}},(err,result)=>{
+          if (err) throw err;
+        })
+      }else{
+        dbo.collection("users").updateOne({_id:new ObjectId(info.id)},{$set:{giudizio:{like:1,dislike:0}}},(err,result)=>{
+          if (err) throw err;
+        })
+      }
+    })
+  })
+})
+app.put("/dislike", async (req,res)=>{
+  let info=JSON.parse(Object.keys(req.body)[0]);
+  MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("targa");
+    dbo.collection("users").find({_id:new ObjectId(info.id)}).toArray(function(err, result) {
+      if(result[0].giudizio){
+        dbo.collection("users").updateOne({_id:new ObjectId(info.id)},{$set:{giudizio:{like:result[0].giudizio.like,dislike:result[0].giudizio.dislike+1}}},(err,result)=>{
+          if (err) throw err;
+        })
+      }else{
+        dbo.collection("users").updateOne({_id:new ObjectId(info.id)},{$set:{giudizio:{like:0,dislike:1}}},(err,result)=>{
+          if (err) throw err;
+        })
+      }
+    })
+  })
+})
+app.post('/profile/:id', upload.single('avatar'), function (req, res, next) {
+  MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("targa");
+    const buffer = Buffer.from(req.file.buffer, "base64");
+    writeFileSync("./images/profileImage"+req.params.id +"."+req.file.mimetype.split("/")[1], buffer)
+    dbo.collection("users").updateOne({_id:new ObjectId(req.params.id)},{$set:{images:{profileImage:"profileImage"+req.params.id +"."+req.file.mimetype.split("/")[1]}}},(err,result)=>{
+      if (err) throw err;
+    })
+  });
+  res.redirect("http://localhost:3000")
+})
+app.get('/fetchImage/:file(*)', (req, res) => {
+  res.sendfile(req.params.file);
+})
+app.put("/coordinate", async (req,res)=>{
+  let info=JSON.parse(Object.keys(req.body)[0]);
+  MongoClient.connect("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority", function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("targa");
+    dbo.collection("users").updateOne({_id:new ObjectId(info.id)},{$set:{coordinate:{latitude:info.latitude,longitude:info.longitude}}},(err,result)=>{
+      if (err) throw err;
+      res.send(result)
     })
   });
 })
